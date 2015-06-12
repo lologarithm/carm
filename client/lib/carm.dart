71f6d6895e1e7b7f9d924d10890346e4f88b7426
Dominic Frost <dominic.frost@workiva.com>
2015-06-12 14:36:53 -0600

    wired up the network

--- client/lib/carm.dart
+++ client/lib/carm.dart
@@ -2,13 +2,4 @@ library carm;
 
 import 'dart:html';
 part 'src/app.dart';
-
-
-class Network {
-  connect() {
-	var ws = new WebSocket('ws://127.0.0.1:9876/ws');
-	ws.send("{}");
-  }
-}
-
-// {"command": "lock"} or {"command": "unlock"}
+part 'src/network.dart';
